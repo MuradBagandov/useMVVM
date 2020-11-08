2f7174c13f00aee60d36a2bcfc5c85ea892253c6
Гаджимурад Багандов <muradb20398@gmail.com>
2020-11-08 15:03:04 +0300

    Создание LambdaCommand

---
+++ UseMVVM/Infrastuctures/Commands/LambdaCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseMVVM.Infrastuctures.Commands.Base;
+
+namespace UseMVVM.Infrastuctures.Commands
+{
+    internal class LambdaCommand:Command
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+
+        public override void Execute(object parameter) => _execute.Invoke(parameter);
+
+
+    }
+}
