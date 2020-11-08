61ae3cff6f7d5055013a2644dfbf9fe6e6f691b0
Гаджимурад Багандов <muradb20398@gmail.com>
2020-11-08 15:16:58 +0300

    Создание комманды закрытия приложения

--- UseMVVM/ViewModels/MainWindowViewModel.cs
+++ UseMVVM/ViewModels/MainWindowViewModel.cs
@@ -3,18 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using UseMVVM.Infrastuctures.Commands;
 using UseMVVM.ViewModels.Base;
 
 namespace UseMVVM.ViewModels
 {
     internal class MainWindowViewModel:ViewModel
     {
+
+        #region Title
         private string _title = "MainTitle";
 
         public string Title
         {
             get => _title;
             set => Set(ref _title, value);
+        }
+        #endregion
+
+        #region Commands
+
+        public ICommand CloseApplicationCommand { get;  }
+
+        public bool CanCloseApplicationCommandExecute(object p) => true;
+
+        public void OnCloseApplicationCommandExecute(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecute, CanCloseApplicationCommandExecute);
         }
 
     }
