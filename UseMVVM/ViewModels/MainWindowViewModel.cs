83cdd1a2ecc7e7f778f237ff39cc3f48708ff10a
Гаджимурад Багандов <muradb20398@gmail.com>
2020-11-08 13:22:38 +0300

    Реализация структуры MVVM паттерна

---
+++ UseMVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseMVVM.ViewModels.Base;
+
+namespace UseMVVM.ViewModels
+{
+    class MainWindowViewModel:ViewModel
+    {
+
+    }
+}
