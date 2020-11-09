0f01b2d9efd84f74c0517d3b632716c7f436c4f4
Гаджимурад Багандов <muradb20398@gmail.com>
2020-11-09 10:47:58 +0300

    Новое окно Calculate

---
+++ UseMVVM/ViewModels/CalculateViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseMVVM.ViewModels
+{
+    class CalculateViewModel: Base.ViewModel
+    {
+        #region Value
+        private string _value;
+
+        public string Value
+        {
+            get => _value;
+            set => Set(ref _value, value);
+        }
+        #endregion
+
+        #region Result
+        private double _result = 0;
+
+        public double Result
+        {
+            get => _result;
+            set => Set(ref _result, value);
+        } 
+        #endregion
+    }
+}
---
+++ UseMVVM/Views/Window/Calculate.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Windows;
+
+namespace UseMVVM.Views.Window
+{
+    /// <summary>
+    /// Логика взаимодействия для Calculate.xaml
+    /// </summary>
+    public partial class Calculate : System.Windows.Window
+    {
+        public Calculate()
+        {
+            InitializeComponent();
+        }
+    }
+}
