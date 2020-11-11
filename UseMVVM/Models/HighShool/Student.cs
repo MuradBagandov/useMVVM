00e800e01fe1bfaa3e909ef0270196f3dce01852
Гаджимурад Багандов <muradb20398@gmail.com>
2020-11-11 17:02:42 +0300

    Студенты и привзяка

---
+++ UseMVVM/Models/HighShool/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseMVVM.Models.HighShool
+{
+
+
+    internal class Student
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public ICollection<int> LastRating { get; set; }
+
+    }
+
+    internal class Group
+    {
+        public string Name { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
--- UseMVVM/ViewModels/MainWindowViewModel.cs
+++ UseMVVM/ViewModels/MainWindowViewModel.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using UseMVVM.Infrastuctures.Commands;
+using UseMVVM.Models.HighShool;
 using UseMVVM.ViewModels.Base;
 
 namespace UseMVVM.ViewModels
 {
     internal class MainWindowViewModel:ViewModel
     {
+        public ObservableCollection<Group> Groups { get; set; }
+
+
+        #region TestList
+        private List<string> _testList = new List<string> { "txt112", "txt12214" };
+
+        public List<string> TestList
+        {
+            get => _testList;
+            set => Set(ref _testList, value);
+        } 
+        #endregion
 
         #region Title
         private string _title = "MainTitle";
@@ -23,6 +37,15 @@ namespace UseMVVM.ViewModels
         }
         #endregion
 
+        private Group _selectedGroup;
+
+        public Group SelectedGroup
+        {
+            get => _selectedGroup;
+            set => Set(ref _selectedGroup, value);
+        }
+       
+
         #region Commands
 
         
@@ -31,7 +54,22 @@ namespace UseMVVM.ViewModels
 
         public MainWindowViewModel()
         {
-            
+            int indexStudent = 1;
+            var students = Enumerable.Range(1, 15).Select(s => new Student
+            {
+                Name = $"Name {indexStudent}",
+                Surname = $"Surname {indexStudent}",
+                Patronymic = $"Patronymic {++indexStudent}",
+                Age = new Random(indexStudent).Next(10, 20),
+                Birthday = DateTime.Today,
+                LastRating = new ObservableCollection<int>(Enumerable.Range(1, 5).Select(x => new Random(x).Next(2, 5)))
+            });
+            var groups = Enumerable.Range(1, 30).Select(x => new Group 
+            { 
+                Name = $"Группа {x}", 
+                Students = new ObservableCollection<Student>(students) 
+            });
+            Groups = new ObservableCollection<Group>(groups);
         }
 
     }
