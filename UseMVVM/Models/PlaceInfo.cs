7a75edd829fe37d3451a79a196aa78bfc1eb6dc6
Гаджимурад Багандов <muradb20398@gmail.com>
2020-12-03 09:46:45 +0300

    Определение модели PlaceInfo и дочерних классов

---
+++ UseMVVM/Models/PlaceInfo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UseMVVM.Models
+{
+    internal abstract class PlaceInfo
+    {
+        public string Name { get; set; }
+
+        public virtual Point Location { get; set; }
+
+        public IEnumerable<ConfirmedCount> Counts { get; set; }
+    }
+
+    internal class CountryInfo : PlaceInfo
+    {
+        private Point _Location;
+        public override Point Location 
+        {
+            get 
+            {
+                if (_Location != null)
+                    return _Location;
+                if (ProvinceCount == null) return default(Point);
+
+                var location_x = ProvinceCount.Average(i => i.Location.X);
+                var location_y= ProvinceCount.Average(i => i.Location.Y);
+
+                return new Point(location_x, location_y);
+            }
+            set => _Location = value; 
+        }
+
+        public IEnumerable<ProvinceInfo> ProvinceCount { get; set; }
+    }
+
+    internal class ProvinceInfo : PlaceInfo {}
+
+    internal struct ConfirmedCount
+    {
+        public DateTime Date { get; set; }
+
+        public int Count { get; set; }
+
+        public ConfirmedCount(DateTime date, int count)
+        {
+            Date = date;
+            Count = count;
+        }
+    }
+
+}
