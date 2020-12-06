984d92bf8ebca3a88bb973f5f9b4669103ae4c9c
Гаджимурад Багандов <muradb20398@gmail.com>
2020-12-06 16:24:26 +0300

    CompositeConvertor

---
+++ UseMVVM/Infrastuctures/Convertors/CompositeConvertor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace UseMVVM.Infrastuctures.Convertors
+{
+    [ValueConversion(typeof(IValueConverter), typeof(IValueConverter))]
+    class CompositeConvertor : Base.Convertor
+    {
+        public IValueConverter First { get; set; }
+
+        public IValueConverter Second { get; set; }
+
+        public override object Convert(object v, Type t, object p, CultureInfo c)
+        {
+            var result_1 = First?.Convert(v, t, p, c) ?? v;
+            var result_2 = Second?.Convert(result_1, t, p, c) ?? result_1;
+            return result_2;
+        }
+
+        public override object ConvertBack(object v, Type t, object p, CultureInfo c)
+        {
+            var result_1 = Second?.ConvertBack(v, t, p, c) ?? v;
+            var result_2 = First?.ConvertBack(result_1, t, p, c) ?? result_1;
+            return result_2;
+        }
+    }
+}
