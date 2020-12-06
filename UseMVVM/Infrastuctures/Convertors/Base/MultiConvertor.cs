7b38629475373fdc1416e66a90702b1c28a3cccb
Гаджимурад Багандов <muradb20398@gmail.com>
2020-12-06 17:11:52 +0300

    MultiConvertor и ToArray

---
+++ UseMVVM/Infrastuctures/Convertors/Base/MultiConvertor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace UseMVVM.Infrastuctures.Convertors.Base
+{
+    internal abstract class MultiConvertor : IMultiValueConverter
+    {
+        public abstract object Convert(object[] values, Type targetType, object parameter, CultureInfo culture);
+
+        public virtual object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
---
+++ UseMVVM/Infrastuctures/Convertors/ToArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseMVVM.Infrastuctures.Convertors
+{
+    class ToArray : Base.MultiConvertor
+    {
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.ToList();
+        }
+    }
+}
