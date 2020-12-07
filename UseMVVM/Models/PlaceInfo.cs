8ee8b8390beb4a665ca00b9a0d046e2188d2eedb
Гаджимурад Багандов <muradb20398@gmail.com>
2020-12-07 20:55:12 +0300

    График по заболеваемости

--- UseMVVM/Models/PlaceInfo.cs
+++ UseMVVM/Models/PlaceInfo.cs
@@ -13,7 +13,7 @@ namespace UseMVVM.Models
 
         public virtual Point Location { get; set; }
 
-        public IEnumerable<ConfirmedCount> Counts { get; set; }
+        public virtual IEnumerable<ConfirmedCount> Counts { get; set; }
 
         public override string ToString()
         {
@@ -28,16 +28,47 @@ namespace UseMVVM.Models
         {
             get 
             {
-                if (_Location != null)
-                    return _Location;
                 if (ProvinceCount == null) return default(Point);
 
                 var location_x = ProvinceCount.Average(i => i.Location.X);
                 var location_y= ProvinceCount.Average(i => i.Location.Y);
 
-                return new Point(location_x, location_y);
+                _Location = new Point(location_x, location_y);
+                return _Location;
             }
             set => _Location = value; 
+        }
+
+        private IEnumerable<ConfirmedCount> _Counts;
+
+        public override IEnumerable<ConfirmedCount> Counts 
+        { 
+            get 
+            {
+                if (ProvinceCount == null) 
+                    return default(IEnumerable<ConfirmedCount>);
+
+                var dates = ProvinceCount.First().Counts.Select(i => i.Date).ToList();
+
+                var counts_provinces = ProvinceCount
+                    .Select(i => i.Counts.Select(c=>c.Count).ToArray())
+                    .ToArray();
+
+                List<int> resultCounts = new List<int>(dates.Count);
+
+                for (int i = 0; i < counts_provinces[0].Length; i++)
+                {
+                    int countSum = 0;
+                    for (int j = 0; j < counts_provinces.Length; j++)
+                    {
+                        countSum += counts_provinces[j][i];
+                    }
+                    resultCounts.Add(countSum);
+                }
+
+                return resultCounts.Zip(dates, (count, date) => new ConfirmedCount(date, count));
+            }
+            set => _Counts = value; 
         }
 
         public IEnumerable<ProvinceInfo> ProvinceCount { get; set; }
