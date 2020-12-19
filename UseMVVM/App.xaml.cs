379f1b4f98e3ad39775c2589a79351d880a0a933
Гаджимурад Багандов <muradb20398@gmail.com>
2020-12-19 14:20:45 +0300

    Создание системы инверсии управления

--- UseMVVM/App.xaml.cs
+++ UseMVVM/App.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
 using UseMVVM.Services;
@@ -14,11 +18,38 @@ namespace UseMVVM
     /// </summary>
     public partial class App : Application
     {
-        public bool IsDesignMode { get; set; } = true;
-        protected override void OnStartup(StartupEventArgs e)
+        public static bool IsDesignMode { get; set; } = true;
+
+        private IHost _host;
+
+        public IHost Host => _host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
         {
-            IsDesignMode = false;
             base.OnStartup(e);
+            IsDesignMode = false;
+            var host = Host;
+            await host.StartAsync().ConfigureAwait(false);
+            host.Dispose();
+            _host = null;
         }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            var host = Host;
+            host.StopAsync().ConfigureAwait(false);
+        }
+
+        internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<DataService>();
+        }
+
+        public static string CurrentDirectory => IsDesignMode == true ?
+            Path.GetDirectoryName(GetSourceCodePath()): 
+            Environment.CurrentDirectory;
+
+        public static string GetSourceCodePath([CallerFilePath]string path = null) => path;
     }
 }
--- UseMVVM/Program.cs
+++ UseMVVM/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace UseMVVM
 {
@@ -14,6 +16,21 @@ namespace UseMVVM
             var app = new App();
             app.InitializeComponent();
             app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] Args)
+        {
+            var host_builder = Host.CreateDefaultBuilder(Args);
+
+            host_builder.UseContentRoot(App.CurrentDirectory);
+            host_builder.ConfigureAppConfiguration((host, cfg) => 
+            {
+                cfg.SetBasePath(App.CurrentDirectory);
+                cfg.AddJsonFile("appSetting.json", true, true);
+            });
+
+            host_builder.ConfigureServices(App.ConfigureServices);
+            return host_builder;
         }
     }
 }
